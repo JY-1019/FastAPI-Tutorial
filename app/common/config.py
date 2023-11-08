5def7b5df8363962891e2cbab5772bfc3d8ac71a
JY-1019 <toy0koy123@gmail.com>
2023-11-09 03:51:28 +0900

    ADD conn.py - Database connection

---
+++ app/common/config.py
@@ -0,0 +1,33 @@
+from dataclasses import dataclass
+from os import path, environ
+
+base_dir = path.dirname(path.dirname(path.dirname(path.abspath(__file__))))
+
+@dataclass
+class Config:
+    """
+    기본 Configuration
+    """
+    BASE_DIR = base_dir
+
+    DB_POOL_RECYCLE: int = 900
+    DB_ECHO: bool = True
+
+@dataclass
+class LocalConfig(Config):
+    PROJ_RELOAD: bool = True
+    DB_URL: str = "mysql+pymysql://gold:rma895623!@localhost:3306/fast_db?charset=utf8"
+
+
+@dataclass
+class ProdConfig(Config):
+    PROJ_RELOAD: bool = False
+
+def conf():
+    """
+    환경 불러오기
+    """
+    config = dict(prod=ProdConfig(), local=LocalConfig())
+    return config.get(environ.get("API_ENV", "local"))
+
+
